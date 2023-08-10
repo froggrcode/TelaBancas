be08c4ebb7141fbe2b7bb524f0c9cf2500c09bed
froggrcode <140923756+froggrcode@users.noreply.github.com>
2023-08-10 10:52:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function TeLaBancas() {
+    
+    if (confirm("Seguro Que Te la Bancas?")) {
+        // Esto se ejecuta si es TRUE
+            alert("Sos un capo te la bancas")
+            prompt("¿Como te llamas?")
+    }
+    else{
+        // Esto se ejcuta si es FALSE
+            alert("No sos un capo, no te la bancas")
+    }
+    
+}
+
+
+
+
+
+
+
